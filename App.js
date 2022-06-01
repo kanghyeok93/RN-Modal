8428ef2ac8cffe7fe72d91fcc75383a9be39b511
SeoKangHyeok <kanghyeok93@naver.com>
2022-06-01 16:29:35 +0900

    feat: Implementing swiper modal component function #1

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import {StyleSheet, SafeAreaView, Button, Text} from 'react-native';
 import React, {useState} from 'react';
 import BasicModal from './src/components/modal/BasicModal';
 import BottomModal from './src/components/modal/BottomModal';
+import SwiperModal from './src/components/modal/SwiperModal';
 
 const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -23,12 +24,19 @@ const App = () => {
         <Text style={styles.title}>I am the baisc modal content!</Text>
       </BasicModal> */}
 
-      <BottomModal
+      {/* <BottomModal
         isVisible={modalVisible}
         onCancel={toggleModal}
         onOK={okModal}>
         <Text style={styles.title}>I am the bottom modal content!</Text>
-      </BottomModal>
+      </BottomModal> */}
+
+      <SwiperModal
+        isVisible={modalVisible}
+        onCancel={toggleModal}
+        onOK={okModal}>
+        <Text style={styles.title}>I am the bottom swiper content!</Text>
+      </SwiperModal>
 
       <Button onPress={() => toggleModal(modalVisible)} title="Modal Click" />
     </SafeAreaView>
---
+++ src/components/modal/SwiperModal.js
@@ -0,0 +1,84 @@
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+import Modal from 'react-native-modal';
+
+/**
+ * @param {Object} object
+ * @param {boolean} object.isVisible
+ * @param {boolean} object.isOneButton
+ * @param {function} object.onCancel
+ * @param {function} object.onOK
+ */
+
+const SwiperModal = ({isVisible, isOneButton, onCancel, onOK, children}) => {
+  const cancelModal = () => {
+    return onCancel(isVisible);
+  };
+
+  const okModal = () => {
+    onCancel(isVisible);
+
+    return onOK();
+  };
+
+  const renderButton = () => {
+    if (!isOneButton) {
+      return (
+        <View style={styles.buttonWrapper}>
+          <TouchableOpacity onPress={cancelModal} style={styles.modalTwoButton}>
+            <Text style={styles.modalCancelText}>Cancel</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={okModal} style={styles.modalTwoButton}>
+            <Text>OK</Text>
+          </TouchableOpacity>
+        </View>
+      );
+    } else {
+      return (
+        <TouchableOpacity onPress={okModal} style={styles.modalOneButton}>
+          <Text>OK</Text>
+        </TouchableOpacity>
+      );
+    }
+  };
+
+  return (
+    <Modal
+      isVisible={isVisible}
+      useNativeDriverForBackdrop
+      onSwipeComplete={cancelModal}
+      swipeDirection={['down']}>
+      <View style={styles.container}>
+        {children}
+
+        {renderButton()}
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 15,
+  },
+  buttonWrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  modalOneButton: {
+    width: '100%',
+    alignItems: 'center',
+    padding: 10,
+  },
+  modalTwoButton: {
+    width: '50%',
+    alignItems: 'center',
+    padding: 10,
+  },
+  modalCancelText: {
+    color: 'red',
+  },
+});
+
+export default SwiperModal;
