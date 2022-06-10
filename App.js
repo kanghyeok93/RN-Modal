e0abc1d76ef03b901110b9869841080d8b84c971
SeoKangHyeok <kanghyeok93@naver.com>
2022-06-10 21:00:39 +0900

    feat: Implementing scroll modal component function #3

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
-import {StyleSheet, SafeAreaView, Button, Text} from 'react-native';
-import React, {useState} from 'react';
+import {
+  StyleSheet,
+  SafeAreaView,
+  ScrollView,
+  View,
+  Button,
+  Text,
+} from 'react-native';
+import React, {useState, useRef} from 'react';
 import BasicModal from './src/components/modal/BasicModal';
 import BottomModal from './src/components/modal/BottomModal';
 import SwiperModal from './src/components/modal/SwiperModal';
@@ -7,6 +14,9 @@ import ScrollModal from './src/components/modal/ScrollModal';
 
 const App = () => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [scrollOffset, setScrollOffset] = useState(null);
+
+  const scrollViewRef = useRef();
 
   const toggleModal = _isVisible => {
     setModalVisible(!_isVisible);
@@ -14,6 +24,10 @@ const App = () => {
 
   const okModal = () => {
     console.log('Modal OK !!!');
+  };
+
+  const handleOnScroll = e => {
+    setScrollOffset(e.nativeEvent.contentOffset.y);
   };
 
   return (
@@ -42,8 +56,24 @@ const App = () => {
       <ScrollModal
         isVisible={modalVisible}
         onCancel={toggleModal}
-        onOK={okModal}>
-        <Text style={styles.title}>I am the bottom swiper content!</Text>
+        onOK={okModal}
+        scrollViewRef={scrollViewRef}
+        scrollOffset={scrollOffset}>
+        <ScrollView
+          ref={scrollViewRef}
+          onScroll={handleOnScroll}
+          scrollEventThrottle={16}>
+          <View style={styles.scrollableModalContent1}>
+            <Text style={styles.scrollableModalText1}>
+              You can scroll me up! 👆
+            </Text>
+          </View>
+          <View style={styles.scrollableModalContent2}>
+            <Text style={styles.scrollableModalText2}>
+              Same here as well! ☝
+            </Text>
+          </View>
+        </ScrollView>
       </ScrollModal>
 
       <Button onPress={() => toggleModal(modalVisible)} title="Modal Click" />
@@ -55,6 +85,26 @@ const styles = StyleSheet.create({
   title: {
     margin: 15,
     textAlign: 'center',
+  },
+  scrollableModalContent1: {
+    height: 200,
+    backgroundColor: '#87BBE0',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollableModalText1: {
+    fontSize: 20,
+    color: 'white',
+  },
+  scrollableModalContent2: {
+    height: 200,
+    backgroundColor: '#A9DCD3',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  scrollableModalText2: {
+    fontSize: 20,
+    color: 'white',
   },
 });
 
